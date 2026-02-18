248a474bcd3cddaca7d721b82abb07c728f7b058
yasml-9 <yasminli3000@gmail.com>
2026-02-19 02:50:19 +0800

    initalising springboot

---
+++ backend/gen_alpha/gen_alpha/src/main/java/csd/g1/t2/gen_alpha/GenAlphaApplication.java
@@ -0,0 +1,13 @@
+package csd.g1.t2.gen_alpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenAlphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenAlphaApplication.class, args);
+	}
+
+}
---
+++ backend/gen_alpha/gen_alpha/src/test/java/csd/g1/t2/gen_alpha/GenAlphaApplicationTests.java
@@ -0,0 +1,13 @@
+package csd.g1.t2.gen_alpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenAlphaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
